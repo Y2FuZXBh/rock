6b20abf505a077c460d424e32bb1269f146fe619
Y2FuZXBh <Y2FuZXBh@proton.me>
2023-02-16 18:14:06 -0800

    test

--- server/setup.ps1
+++ server/setup.ps1
@@ -1,3 +1,4 @@
 # Server - Setup Script
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 Install-Module -Name PSWindowsUpdate -Force
