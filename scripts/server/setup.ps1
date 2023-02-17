8b69374e4f911595f09e508fb012ef4593e330a4
Y2FuZXBh <Y2FuZXBh@proton.me>
2023-02-16 18:45:53 -0800

    testing enable feature & DockerMsftProvider

--- scripts/server/setup.ps1
+++ scripts/server/setup.ps1
@@ -3,5 +3,8 @@ Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Confirm:$false
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted 
 Install-Module -Name ("PSWindowsUpdate", "DockerMsftProvider") -Confirm:$false
 Set-PSRepository -Name PSGallery -InstallationPolicy Untrusted
+Install-Package -Name docker -ProviderName DockerMsftProvider -Confirm:$false
 Import-Module -Name PSWindowsUpdate -Function Get-WUInstall
-Get-WUInstall –MicrosoftUpdate –AcceptAll –AutoReboot
+Enable-WindowsOptionalFeature -Online -FeatureName Containers -Confirm:$false
+Get-WUInstall –MicrosoftUpdate –AcceptAll
+#Restart-Computer -Force
