37e2e02a6bbd95af8fff278e6575d771d381438b
Y2FuZXBh <Y2FuZXBh@proton.me>
2023-02-16 18:19:48 -0800

    test

--- server/setup.ps1
+++ server/setup.ps1
@@ -1,5 +1,6 @@
 # Server - Setup Script
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-Install-Module -Name PSWindowsUpdate -Force
+Install-Module -Name PSWindowsUpdate
+Import-Module -Name PSWindowsUpdate -Function Get-WUInstall
 Get-WUInstall –MicrosoftUpdate –AcceptAll –AutoReboot
