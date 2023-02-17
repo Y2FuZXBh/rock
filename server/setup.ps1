a10e5098f90d8818403110551291144f95bb090a
Y2FuZXBh <Y2FuZXBh@proton.me>
2023-02-16 18:21:30 -0800

    setting to untrusted after pull

--- server/setup.ps1
+++ server/setup.ps1
@@ -2,5 +2,6 @@
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 Install-Module -Name PSWindowsUpdate
+Set-PSRepository -Name PSGallery -InstallationPolicy Untrusted
 Import-Module -Name PSWindowsUpdate -Function Get-WUInstall
 Get-WUInstall –MicrosoftUpdate –AcceptAll –AutoReboot
