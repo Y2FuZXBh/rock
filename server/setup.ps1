f8d1e08903b748bff54d9f7c15d7da2dfb3defb7
Y2FuZXBh <Y2FuZXBh@proton.me>
2023-02-16 18:16:50 -0800

    testing update command

--- server/setup.ps1
+++ server/setup.ps1
@@ -2,3 +2,4 @@
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 Install-Module -Name PSWindowsUpdate -Force
+Get-WUInstall –MicrosoftUpdate –AcceptAll –AutoReboot
