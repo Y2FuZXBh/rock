c84fd7a843f86cfc601c18a67b0319349a57c36f
Y2FuZXBh <Y2FuZXBh@proton.me>
2023-02-16 18:49:39 -0800

    added for for install

--- scripts/server/setup.ps1
+++ scripts/server/setup.ps1
@@ -4,7 +4,7 @@ Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 Install-Module -Name ("PSWindowsUpdate", "DockerMsftProvider") -Confirm:$false
 Set-PSRepository -Name PSGallery -InstallationPolicy Untrusted
 Install-Package -Name docker -ProviderName DockerMsftProvider -Confirm:$false
-Import-Module -Name PSWindowsUpdate -Function Get-WUInstall
+Import-Module -Name PSWindowsUpdate -Function Get-WUInstall -Force
 Enable-WindowsOptionalFeature -Online -FeatureName Containers -Confirm:$false
 Get-WUInstall –MicrosoftUpdate –AcceptAll
 #Restart-Computer -Force
