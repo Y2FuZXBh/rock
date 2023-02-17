5a21d0936840e9a02fdf44a109ea82564907768f
Y2FuZXBh <Y2FuZXBh@proton.me>
2023-02-16 18:05:16 -0800

    adding setup script for server for testing

---
+++ runner/setup.ps1
@@ -0,0 +1,9 @@
+# CircleCI - Runner
+## https://circleci.com/docs/runner-overview/
+$install = https://raw.githubusercontent.com/CircleCI-Public/runner-installation-files/main/windows-install/Install-CircleCIRunner.ps1
+$uninstall = https://raw.githubusercontent.com/CircleCI-Public/runner-installation-files/main/windows-install/Uninstall-CircleCIRunner.ps1
+
+Set-ExecutionPolicy Bypass -Scope Process -Force
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+
+#Invoke-WebRequest -UseBasicParsing -Uri $runner_install
---
+++ server/setup.ps1
@@ -0,0 +1,3 @@
+# Server - Setup Script
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Install-Module -Name PSWindowsUpdate
