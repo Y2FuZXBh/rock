c0b0f1cb6823867fb251b76bebc5c36bf88b70e3
Y2FuZXBh <Y2FuZXBh@proton.me>
2023-02-16 20:51:35 -0800

    testing chocolatey

--- scripts/server/setup.ps1
+++ scripts/server/setup.ps1
@@ -1,10 +1,20 @@
-# Server - Setup Script
+# Server - Install Script
+
+# install & run windows update
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Confirm:$false
-Set-PSRepository -Name PSGallery -InstallationPolicy Trusted 
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 Install-Module -Name ("PSWindowsUpdate", "DockerMsftProvider") -Confirm:$false
 Set-PSRepository -Name PSGallery -InstallationPolicy Untrusted
-Install-Package -Name docker -ProviderName DockerMsftProvider -Update -Confirm:$false
 Import-Module -Name PSWindowsUpdate -Function Get-WUInstall -Force
-Enable-WindowsOptionalFeature -Online -FeatureName Containers -Confirm:$false
 Get-WUInstall –MicrosoftUpdate –AcceptAll
+
+# install docker
+Install-Package -Name docker -ProviderName DockerMsftProvider -Update -Confirm:$false
+Enable-WindowsOptionalFeature -Online -FeatureName Containers -Confirm:$false
+
+# add windows reboot check here
 #Restart-Computer -Force
+
+# chocolatey
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+wget -UseBasicParsing https://community.chocolatey.org/install.ps1 | iex
