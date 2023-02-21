207eb71df86739171d109ee33c17beb34dc05723
Y2FuZXBh <Y2FuZXBh@proton.me>
2023-02-21 13:19:27 -0800

    testing runner

--- scripts/runner/setup.ps1
+++ scripts/runner/setup.ps1
@@ -3,12 +3,14 @@
 # install
 choco install -y git gh circleci-cli
 
-# update
-circleci update
+# pass user read token & auth through git
+gc Z:\share\pat.txt | gh auth login --with-token $_
 
-# pass user read token
-# auth through git 
 # pull secret with gh
+#gh secret list --repo github.com/Y2FuZXBh/rock
+#gh api /repos/Y2FuZXBh/rock/actions/secrets/CIRCLECI_RUNNER_WINDOWS22
+
 # copy & replace template(s)
 
-# setup circleci connection
+# setup circleci connection (add this with built-in token+name)
+(wget -UseBasicParsing https://raw.githubusercontent.com/CircleCI-Public/runner-installation-files/main/windows-install/Install-CircleCIRunner.ps1).content | iex
