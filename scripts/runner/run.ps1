5bbf54578508f50b0ce5dc69813c64e54319fcfc
Y2FuZXBh <Y2FuZXBh@proton.me>
2023-03-11 17:12:58 -0800

    adding tester file for webapp

--- scripts/runner/run.ps1
+++ scripts/runner/run.ps1
@@ -1,2 +1,9 @@
 # run docker containers here after build tests...
 
+$USERNAME = $env:username
+
+Set-Location ~/Desktop
+
+docker build . --force-rm --pull --compress --tag rock:latest
+
+docker run --detach --name "rock-$USERNAME" -p 80:80 -p 443:443 rock:latest
